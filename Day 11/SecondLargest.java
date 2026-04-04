40e7ae88947d06263c5a0e90548562d9a8d1353a
Kumari Priyanshu <kripriyanshu0212@gmail.com>
2026-04-04 11:11:10 +0530

    Day 11/90 – Array Practice

---
+++ Day 11/SecondLargest.java
@@ -0,0 +1,23 @@
+// Program to find the second largest element in an array.
+public class SecondLargest {
+    public static void main(String[] args) {
+        int []arr = {10,5,20,8,15};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] > secondLargest && arr[i] != largest){
+                secondLargest = arr[i];
+            }
+        }
+        if(secondLargest == Integer.MIN_VALUE){
+            System.out.println("No second largest element");
+        }
+        else{
+            System.out.println("Second largest = " + secondLargest);
+        }
+    }
+}
---
+++ Day 11/Sorted.java
@@ -0,0 +1,20 @@
+public class Sorted {
+    public static void main(String[] args) {
+        int []arr = {5, 2, 3, 4, 1};
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+    }
+}
